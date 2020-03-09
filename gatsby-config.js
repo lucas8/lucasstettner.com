a0d724ef5a38ddcfc0e9044e973b6c6e2b6f6f37
Lucas Stettner <lebra448@gmail.com>
2020-03-09 19:28:19 -0400

    added analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     siteName: `Lucas Stettner Personal Site`,
   },
-  plugins: [`gatsby-plugin-typescript`, `gatsby-plugin-styled-components`],
+  plugins: [
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-styled-components`,
+    {
+      resolve: `gatsby-plugin-fathom`,
+      options: {
+        siteId: `XUERLOZP`,
+      },
+    },
+  ],
 }
